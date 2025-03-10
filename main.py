a3c0efa348ca9e817d0db6cad61c65fe0f1eb48c
sahar58600 <94462339+sahar58600@users.noreply.github.com>
2025-03-10 09:48:15 +0200

    removed #

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()
 print('started')
 
 if __name__ == "__main__":
-    container_name = "flight"  # Replace with your container name
-    prefix = "flight/2024-03-09_00-00-00/"  # Replace with desired blob prefix or leave as an empty string
+    container_name = "flight"
+    prefix = "flight/2024-03-09_00-00-00/"
 
     list_dynamic_blobs(container_name, prefix)
